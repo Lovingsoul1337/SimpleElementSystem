360c921e8495211bd7db46c16693f84b333c88e4
Michael <lovingsoul1337@gmail.com>
2020-05-10 19:31:00 +0200

    -fixed rectangle ninja !

--- main.lua
+++ main.lua
@@ -12,15 +12,6 @@ function love.load()
    image = love.graphics.newImage('Assets/Images/rectangle_ninja.png'), --if you want it to be drawed add a image and position value !
    position = {x = 88, y = 88}})
 
-   world:createEntity({  --second add a entity including values !
-    --id = 1,
-    name = "Fero",
-    health = "77",
-    maxHealth = "111",
-    --input = {"leftstick", "rightstick", "x", "y", "b", "a", "leftb", "rightb", "leftt", "rightt", "dpad", "start", "select", "special"}
-    image = love.graphics.newImage('Assets/Images/rectangle_ninja.png'), --if you want it to be drawed add a image and position value !
-    position = {x = 22, y = 22}})
-
 end
 
 function love.update(dt)
