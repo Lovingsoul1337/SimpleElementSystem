83cedebbab36c8d5092367a159c579311fc65f59
Michael <lovingsoul1337@gmail.com>
2020-05-10 23:26:34 +0200

    -fixed 2 bugs ! thanks to tjakka5 3>

--- main.lua
+++ main.lua
@@ -4,23 +4,25 @@ function love.load()
 
   world = world:new()   --create a new instance of world
   world:createEntity({  --second add a entity including values !
-   --id = 1,
    name = "Hero",
    health = "70",
    maxHealth = "100",
-   --input = {"leftstick", "rightstick", "x", "y", "b", "a", "leftb", "rightb", "leftt", "rightt", "dpad", "start", "select", "special"}
    image = love.graphics.newImage('Assets/Images/rectangle_ninja.png'), --if you want it to be drawed add a image and position value !
    position = {x = 88, y = 88}})
 
    world:createEntity({  --second add a entity including values !
-    --id = 1,
     name = "Fero",
     health = "70",
     maxHealth = "100",
-    --input = {"leftstick", "rightstick", "x", "y", "b", "a", "leftb", "rightb", "leftt", "rightt", "dpad", "start", "select", "special"}
     image = love.graphics.newImage('Assets/Images/rectangle_ninja.png'), --if you want it to be drawed add a image and position value !
     position = {x = 188, y = 188}})
 
+    world:createEntity({  --second add a entity including values !
+     name = "Nice",
+     health = "88",
+     maxHealth = "100",
+     image = love.graphics.newImage('Assets/Images/rectangle_ninja.png'), --if you want it to be drawed add a image and position value !
+     position = {x = 288, y = 288}})
 end
 
 function love.update(dt)
--- systemmanager.lua
+++ systemmanager.lua
@@ -6,35 +6,33 @@ systemmanager.systems = littlehelper.requireSystems()
 systemmanager.drawingQueue = {}
 
 function systemmanager:update(entity, dt)
-
   if systemmanager.checkIfDrawable(entity) then
     table.insert(systemmanager.drawingQueue, entity)
   end
 
   for k1, v1 in pairs(systemmanager.systems) do
-    if systemmanager.checkEntity(entity, systemmanager.systems[k1]) then
-      v1:update(entity, dt)
+    if systemmanager.checkEntity(entity, systemmanager.systems[k1]["requiredElements"]) then
+       v1:update(entity, dt)
     end
   end
 end
 
 function systemmanager.checkEntity(entity, requiredComponents)
-  for key, component in ipairs(requiredComponents) do
-    if not entity[key] then
+  for k1, v1 in pairs(requiredComponents) do
+    if entity[v1] == nil then
       return false
     end
   end
-    return true
+  return true
 end
 
 function systemmanager.checkIfDrawable(entity)
   for k, v in pairs(entity) do
     if k == "image" then
       return true
-    else
-      return false
     end
    end
+  return false
 end
 
 function systemmanager:draw(entity)
